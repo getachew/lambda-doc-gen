6dab617a17ef832aababeb1749345880bb57f179
getachew <abel.getachew@capitalone.com>
2017-12-06 06:49:29 -0500

    added comments

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 	"net/http"
 	"os"
 
+	// using https://github.com/apex/up for Amazon Web Services (AWS)
+	// AWS Lambda is a simple, cost effective solution to host such service
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/json"
 	"github.com/apex/log/handlers/text"
 
+	// using this open source software to manipulate the template
 	"github.com/nguyenthenguyen/docx"
 )
 
+// The html form lives here
 var views = template.Must(template.ParseGlob("views/*.html"))
 
 // use JSON logging when run by Up (including `up start`).
@@ -26,7 +30,11 @@ func init() {
 // setup.
 func main() {
 	addr := ":" + os.Getenv("PORT")
+
+	// when submit button on the html is pressed the submit function (line 61)  is invoked
 	http.HandleFunc("/submit", submit)
+
+	// this handles the index or landing page currently the form
 	http.HandleFunc("/", index)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatalf("error listening: %s", err)
@@ -35,18 +43,12 @@ func main() {
 
 // index page.
 func index(w http.ResponseWriter, r *http.Request) {
-	name := cookie(r, "name")
-	email := cookie(r, "email")
-
 	w.Header().Set("Content-Type", "text/html")
 
 	views.ExecuteTemplate(w, "index.html", struct {
 		Name  string
 		Email string
-	}{
-		Name:  name,
-		Email: email,
-	})
+	}{})
 }
 
 // submit handler.
@@ -59,27 +61,34 @@ func submit(w http.ResponseWriter, r *http.Request) {
 		"email": email,
 	}).Info("submit")
 
-	http.SetCookie(w, &http.Cookie{
-		Name:  "name",
-		Value: name,
-	})
-
 	doc, err := docx.ReadDocxFile("views/Form1.docx")
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
+	defer doc.Close()
 
 	docx1 := doc.Editable()
+
+	// This is where we start replacing the template document with data from the html form
+	// the word document template has place holders like %name% that map to the HTML form "name"
 	docx1.Replace("%name%", name, -1)
 	docx1.Replace("%email%", email, -1)
 
+	// this sets the correct header for a microsoft word document download
 	w.Header().Set("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+
+	// this names the downloaded file to form1.docx
 	w.Header().Set("Content-Disposition", "attachment; filename=form1.docx")
-	docx1.Write(w)
 
-	doc.Close()
+	// this will make the download start
+	err = docx1.Write(w)
 
-	redirectBack(w, r)
+	// if we error out in starting the download
+	if err != nil {
+		// we log the error for debugging
+		log.Errorf("Error writing file: %v", err)
+	}
+
 }
 
 // redirect to referrer helper.
