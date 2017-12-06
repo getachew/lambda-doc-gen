67be40a649b58cf968eb73e261edef04ed928a59
getachew <abel.getachew@capitalone.com>
2017-12-06 06:36:58 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/json"
+	"github.com/apex/log/handlers/text"
+
+	"github.com/nguyenthenguyen/docx"
+)
+
+var views = template.Must(template.ParseGlob("views/*.html"))
+
+// use JSON logging when run by Up (including `up start`).
+func init() {
+	if os.Getenv("UP_STAGE") == "" {
+		log.SetHandler(text.Default)
+	} else {
+		log.SetHandler(json.Default)
+	}
+}
+
+// setup.
+func main() {
+	addr := ":" + os.Getenv("PORT")
+	http.HandleFunc("/submit", submit)
+	http.HandleFunc("/", index)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("error listening: %s", err)
+	}
+}
+
+// index page.
+func index(w http.ResponseWriter, r *http.Request) {
+	name := cookie(r, "name")
+	email := cookie(r, "email")
+
+	w.Header().Set("Content-Type", "text/html")
+
+	views.ExecuteTemplate(w, "index.html", struct {
+		Name  string
+		Email string
+	}{
+		Name:  name,
+		Email: email,
+	})
+}
+
+// submit handler.
+func submit(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+
+	log.WithFields(log.Fields{
+		"name":  name,
+		"email": email,
+	}).Info("submit")
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "name",
+		Value: name,
+	})
+
+	doc, err := docx.ReadDocxFile("views/Form1.docx")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	docx1 := doc.Editable()
+	docx1.Replace("%name%", name, -1)
+	docx1.Replace("%email%", email, -1)
+
+	w.Header().Set("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+	w.Header().Set("Content-Disposition", "attachment; filename=form1.docx")
+	docx1.Write(w)
+
+	doc.Close()
+
+	redirectBack(w, r)
+}
+
+// redirect to referrer helper.
+func redirectBack(w http.ResponseWriter, r *http.Request) {
+	url := r.Header.Get("Referer")
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+// cookie helper.
+func cookie(r *http.Request, name string) string {
+	c, err := r.Cookie(name)
+	if err != nil {
+		return ""
+	}
+
+	return c.Value
+}
